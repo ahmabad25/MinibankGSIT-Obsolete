5ff24f9e54d00dc6a424ef1a70bb4c055223148e
Bituser <bituser@bca>
2020-01-23 09:49:58 +0700

    First Upload

---
+++ src/main/java/com/example/demo/MinibankGsitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibankGsitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinibankGsitApplication.class, args);
+	}
+
+}
